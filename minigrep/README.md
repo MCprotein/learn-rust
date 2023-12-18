448c8a173a145c35ed6ce57fb05eff959dd4d9d0
MCprotein <chss3339@gmail.com>
2023-12-18 22:10:56 +0900

    feat(cli): cli 인자 받기

---
+++ minigrep/README.md
@@ -0,0 +1,3 @@
+```bash
+$ cargo run -- searchstring example-filename.txt
+```
---
+++ minigrep/src/main.rs
@@ -0,0 +1,18 @@
+use std::env;
+
+fn main() {
+    /* collect는 반복자가 가진 모든 값들을 벡터 형태로 반환한다.
+       많은 종류의 콜렉션들이 사용될 수 있기때문에 String타입이라고 명시한다.
+       dbg! = 디버그 매크로
+
+       args[0] = 실행 파일의 경로와 이름
+       args[1] = 첫번째 인수
+    */
+    let args: Vec<String> = env::args().collect();
+
+    let query = &args[1];
+    let file_path = &args[2];
+
+    println!("Searching for {query}");
+    println!("In file {file_path}");
+}
