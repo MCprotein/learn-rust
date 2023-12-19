288fabb6ade5b14b33d7a11a9996cfa28df7cc86
MCprotein <chss3339@gmail.com>
2023-12-19 22:17:48 +0900

    feat(cli): txt 파일 열기

--- minigrep/src/main.rs
+++ minigrep/src/main.rs
@@ -1,4 +1,5 @@
 use std::env;
+use std::fs;
 
 fn main() {
     /* collect는 반복자가 가진 모든 값들을 벡터 형태로 반환한다.
@@ -15,4 +16,8 @@ fn main() {
 
     println!("Searching for {query}");
     println!("In file {file_path}");
+
+    let contents = fs::read_to_string(file_path).expect("Should have been able to read the file");
+
+    println!("With text:\n{contents}");
 }
