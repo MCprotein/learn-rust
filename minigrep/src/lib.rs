2f5f1600c618876111ddc90a75b7a27d12248e9e
MCprotein <chss3339@gmail.com>
2023-12-19 23:12:35 +0900

    feat(cli): test 추가

--- minigrep/src/lib.rs
+++ minigrep/src/lib.rs
@@ -22,7 +22,40 @@ impl Config {
 pub fn run(config: Config) -> Result<(), Box<dyn Error>> {
     let contents = fs::read_to_string(config.file_path)?;
 
-    println!("With text:\n{contents}");
+    for line in search(&config.query, &contents) {
+        println!("{line}");
+    }
 
     Ok(())
+}
+
+/*
+  search 함수로 반환되는 데이터는 search 함수로 전달된 contents 인자만큼 오래 유지될것라는 의미
+  search 반환값은 query가 아니라 contents로부터 만들었기때문에 컴파일러가 query에서 만들었다고 착각하지 않게 함
+*/
+pub fn search<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
+    let mut results = Vec::new();
+
+    for line in contents.lines() {
+        if line.contains(query) {
+            results.push(line);
+        }
+    }
+
+    results
+}
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn one_result() {
+        let query = "duct";
+        let contents = "\
+Rust:
+safe, fast, productive.
+Pick three.";
+
+        assert_eq!(vec!["safe, fast, productive."], search(query, contents));
+    }
 }
