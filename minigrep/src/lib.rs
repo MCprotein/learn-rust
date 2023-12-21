b4140ff522a2f515e0fbfabedb2388e23fa76b06
yoonsooshin <yoonsoo@jentestore.com>
2023-12-21 18:07:35 +0900

    fix(doc): 문서 주석에 테스트 추가

--- minigrep/src/lib.rs
+++ minigrep/src/lib.rs
@@ -115,3 +115,18 @@ Trust me.";
         );
     }
 }
+
+/// Adds one to the number given.
+///
+/// # 예제
+/// 아래 예제는 cargo test 하면 실행된다.
+///
+/// ```
+/// let arg = 5;
+/// let answer = minigrep::add_one(arg);
+///
+/// assert_eq!(6, answer);
+/// ````
+pub fn add_one(x: i32) -> i32 {
+    x + 1
+}
