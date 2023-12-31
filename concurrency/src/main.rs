c8bc510c14a05991032840c8b4cf0e03894b11f9
MCprotein <chss3339@gmail.com>
2024-01-01 01:33:31 +0900

    feat(concurrency): thread join 사용

--- concurrency/src/main.rs
+++ concurrency/src/main.rs
@@ -5,7 +5,7 @@ fn main() {
     /*
        thread::spawn으로 새로운 스레드를 생성한다.
     */
-    thread::spawn(|| {
+    let handle = thread::spawn(|| {
         for i in 1..10 {
             println!("hi number {} from the spawned thread!", i);
             /*
@@ -15,7 +15,7 @@ fn main() {
             thread::sleep(Duration::from_millis(1));
         }
     });
-
+    // handle.join().unwrap();
     for i in 1..5 {
         println!("hi number {} from the main thread!", i);
         thread::sleep(Duration::from_millis(1));
@@ -26,5 +26,8 @@ fn main() {
        thread::spawn의 반환 타입은 JoinHandle이다.
        JoinHandle은 join 메소드를 호출했을때 스레드가 끝날때까지 기다릴 수 있는 소유된 값이다.
 
+        join()을 호출하면 현재 실행중인 스레드가 join한 스레드가 종료될때까지 차단된다. 즉, 스레드가 끝날때까지 blocking 한다.
+        스레드 차단: 작업을 수행하거나 종료되는것 방지
     */
+    handle.join().unwrap();
 }
